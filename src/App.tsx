7da4c47ad697393222552ff256cb8ce382e481b3
reallynotburner <reallynotburner@gmail.com>
2020-01-23 07:46:02 -0500

    minimal boilerplace for web, ios, android

--- src/App.tsx
+++ src/App.tsx
@@ -12,54 +12,21 @@ import { StyleSheet, Text, View } from 'react-native';
 
 const App = () => {
   return (
-    <View style={styles.scrollView}>
-      <Text style={styles.sectionDescription}>
-        Edit <Text style={styles.highlight}>App.js</Text> to change this
-        screen and then come back to see your edits.</Text>
+    <View style={styles.appContainer}>
+      <Text>
+        Edit App.js to change this
+        screen.</Text>
     </View>
-
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    flexDirection: 'column',
+  appContainer: {
     flex: 1,
-    backgroundColor: 'purple'
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: 'white',
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: 'black',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: 'orange',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: 'pink',
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: 'pink'
+  }
 });
 
 export default App;
